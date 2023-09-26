882b701e6c046a18683405b6c8de433d6f839f53
Victor Ciolac <victor.bcfraga@gmail.com>
2023-09-26 00:20:58 -0300

    Iniciando projeto, normalizado, criando tabelas

--- challenges/desafio1.sql
+++ challenges/desafio1.sql
@@ -1,23 +1,21 @@
--- Descomente e altere as linhas abaixo:
+DROP DATABASE IF EXISTS SpotifyClone;
+CREATE DATABASE IF NOT EXISTS SpotifyClone;
+CREATE TABLE SpotifyClone.albums
+(
+    album_id	INT PRIMARY KEY,
+    album_name	VARCHAR(512) NOT NULL,
+    artista_id	INT,
+    ano_lancamento	INT,
+    FOREIGN KEY (artista_id) REFERENCES SpotifyClone.artistas(artista_id)
+);
 
--- DROP DATABASE IF EXISTS SpotifyClone;
--- CREATE DATABASE IF NOT EXISTS SpotifyClone;
--- CREATE TABLE SpotifyClone.tabela1(
---     coluna1 tipo restricoes,
---     coluna2 tipo restricoes,
---     colunaN tipo restricoes,
--- ) engine = InnoDB;
--- CREATE TABLE SpotifyClone.tabela2(
---     coluna1 tipo restricoes,
---     coluna2 tipo restricoes,
---     colunaN tipo restricoes,
--- ) engine = InnoDB;
--- INSERT INTO SpotifyClone.tabela1 (coluna1, coluna2)
--- VALUES
---   ('exemplo de dados 1', 'exemplo de dados A'),
---   ('exemplo de dados 2', 'exemplo de dados B'),
---   ('exemplo de dados 3', 'exemplo de dados C');
--- INSERT INTO SpotifyClone.tabela2 (coluna1, coluna2)
--- VALUES
---   ('exemplo de dados 1', 'exemplo de dados X'),
---   ('exemplo de dados 2', 'exemplo de dados Y');
+INSERT INTO SpotifyClone.albums (album_id, album_name, artista_id, ano_lancamento)
+VALUES 
+    ('1', 'Renaissance', '1', '2022'),
+    ('2', 'Jazz', '2', '1978'),
+    ('3', 'Hot Space', '2', '1982'),
+    ('4', 'Falso Brilhante', '3', '1998'),
+    ('5', 'Vento de Maio', '3', '2001'),
+    ('6', 'QVVJFA?', '4', '2003'),
+    ('7', 'Somewhere Far Beyond', '5', '2007'),
+    ('8', 'I Put A Spell On You', '6', '2012');
